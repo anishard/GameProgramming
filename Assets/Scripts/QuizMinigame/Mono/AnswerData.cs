89ad6e20b8d8aa1a5f9af2b088d8e3063fcff6f1
Claire <claireava7243@gmail.com>
2024-12-08 15:59:12 -0500

    Memory game

---
+++ Assets/Scripts/QuizMinigame/Mono/AnswerData.cs
@@ -0,0 +1,73 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnswerData : MonoBehaviour {
+
+    #region Variables
+
+    [Header("UI Elements")]
+    [SerializeField] TextMeshProUGUI infoTextObject = null;
+    [SerializeField] Image toggle = null;
+
+    [Header("Textures")]
+    [SerializeField] Sprite uncheckedToggle = null;
+    [SerializeField] Sprite checkedToggle = null;
+
+    [Header("References")]
+    [SerializeField] GameEvents events = null;
+
+    private RectTransform _rect = null;
+    public RectTransform Rect
+    {
+        get
+        {
+            if (_rect == null)
+            {
+                _rect = GetComponent<RectTransform>() ?? gameObject.AddComponent<RectTransform>();
+            }
+            return _rect;
+        }
+    }
+
+    private int _answerIndex = -1;
+    public int AnswerIndex { get { return _answerIndex; } }
+
+    private bool Checked = false;
+
+    #endregion
+
+    //updates the answer data
+    public void UpdateData (string info, int index)
+    {
+        infoTextObject.text = info;
+        _answerIndex = index;
+    }
+    
+    //resets values back to default
+    public void Reset ()
+    {
+        Checked = false;
+        UpdateUI();
+    }
+    
+    //switches state
+    public void SwitchState ()
+    {
+        Checked = !Checked;
+        UpdateUI();
+
+        if (events.UpdateQuestionAnswer != null)
+        {
+            events.UpdateQuestionAnswer(this);
+        }
+    }
+    
+    //updates ui
+    void UpdateUI ()
+    {
+        if (toggle == null) return;
+
+        toggle.sprite = (Checked) ? checkedToggle : uncheckedToggle;
+    }
+}
