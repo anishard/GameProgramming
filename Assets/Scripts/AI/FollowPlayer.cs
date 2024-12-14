fba7bb5a66d34fd01ea8366c1456d343b02e692b
Claire <claireava7243@gmail.com>
2024-12-14 03:38:34 -0500

    added lost found game outro dialogue and monkey steals items from inventory

--- Assets/Scripts/AI/FollowPlayer.cs
+++ Assets/Scripts/AI/FollowPlayer.cs
@@ -11,6 +11,8 @@ public class FollowPlayer : MonoBehaviour
     private NavMeshAgent agent;
     private bool isChasing = false;
     public float agentSpeed = 3.5f;
+    public GameObject muskrat;
+    //public bool hasBeenCaught;
 
     // Start is called before the first frame update
     void Start()
@@ -35,13 +37,21 @@ public class FollowPlayer : MonoBehaviour
     /////////////////////////////////////////////////////
 
     public void ChasePlayer() {
-        // float distNPCToPlayer = Vector3.Distance(transform.position, player.position);
-
-        // //if the NPC comes within a certain radius of the player, start following the player
-        // if (distNPCToPlayer <= radiusToFollow) {
-        //     agent.destination = player.position;
-        // }
         isChasing = true;
+    }
+
+    public void StopChasePlayer() {
+        isChasing = false;
+    }
+    // if monkey collides with player
+    void OnCollisionEnter(Collision collision) {
+        LostFoundGame lfScript = muskrat.GetComponent<LostFoundGame>();
+
+        // if game has started and player collides with monkey, end game
+        if (collision.gameObject.CompareTag("Player") && lfScript.hasPlayedIntroDialogue) {
+            // lfScript.StopLostFoundGame();
+            lfScript.StealFromInventory();
+        }
     }
     //to see the radius around the player that the NPC has to be in to start following it
     // void OnDrawGizmosSelected() {
--- Assets/Scripts/AI/LostFoundGame.cs
+++ Assets/Scripts/AI/LostFoundGame.cs
@@ -5,22 +5,26 @@ using UnityEngine.AI;
 
 public class LostFoundGame : MonoBehaviour
 {
-    private bool hasPlayedIntroDialogue;
+    public bool hasPlayedIntroDialogue;
 
     public GameObject spawnedItem;
     public float spawnRadius = 18f;
     public GameObject forkSpawner;
     public GameObject monkey;
 
+    Inventory inventory;
+
     // Start is called before the first frame update
     void Start()
     {
         hasPlayedIntroDialogue = false;
+        inventory = Inventory.instance;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //TODO: if the fork is put in the inventory, call StopLostFoundGame()
         
     }
 
@@ -30,7 +34,7 @@ public class LostFoundGame : MonoBehaviour
         }
     }
 
-    void StartLostFoundGame() {
+    public void StartLostFoundGame() {
         if (!hasPlayedIntroDialogue) {
             // play the opening dialogue, and make sure the dialogue doesn't replay after it ends
             Dialogue.Activate("LostFoundIntro");
@@ -41,13 +45,40 @@ public class LostFoundGame : MonoBehaviour
 
             // have the monkey start chasing the player (if player goes within certain radius of monkey)
             FollowPlayer fpScript = monkey.GetComponent<FollowPlayer>();
-            if (fpScript != null) {
+            if (fpScript != null) { // && fork hasn't been found
                 fpScript.ChasePlayer();
             }
             else {
                 Debug.Log("the script is not found");
             }
+
+            // if player was caught
+            // if (fpScript.hasBeenCaught) {
+            //     Debug.Log("YOU HAVE BEEN CAUGHT BY MONKEY");
+            //     fpScript.StopChasePlayer();
+            // }
         }
+    }
+
+    public void StopLostFoundGame() {
+        //stop the game
+        Debug.Log("You found the muskrat's pitchfork! Good job!");
+
+        //stop monkey from chasing you
+        FollowPlayer fpScript = monkey.GetComponent<FollowPlayer>();
+        if (fpScript != null) { // && fork hasn't been found
+            fpScript.StopChasePlayer();
+        }
+        else {
+            Debug.Log("the script is not found");
+        }
+
+        Dialogue.Activate("LostFoundOutro");
+
+        // TODO: Collect the coins reward
+
+
+
     }
 
     private void SpawnFork() {
@@ -58,4 +89,23 @@ public class LostFoundGame : MonoBehaviour
             Instantiate(spawnedItem, hit.position, Quaternion.identity);
         }
     }
+
+    public void StealFromInventory() {
+        //if monkey collides with player, take something out of inventory
+        int randInt = Random.Range(0, inventory.items.Count);
+
+        if (inventory.items.Count == 0) {
+            Debug.Log("You don't have any items in your inventory for the monkey to steal...He'll still chase you though!");
+        }
+        else {
+            Debug.Log("Oh no! That monkey stole a " + inventory.items[randInt].name + " from you!");
+
+            inventory.Remove(inventory.items[randInt]);
+        }
+        
+        //TODO: activate UI with muskrat saying "Oh no! The monkey is taking your stuff!
+
+
+    }
+
 }
