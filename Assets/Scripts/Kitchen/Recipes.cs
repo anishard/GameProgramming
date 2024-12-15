c5cb910e0012f8a522a5a8912596c86433526436
Eric Fournier <edfournier@umass.edu>
2024-12-15 18:38:01 -0500

    recipe hint

--- Assets/Scripts/Kitchen/Recipes.cs
+++ Assets/Scripts/Kitchen/Recipes.cs
@@ -1,16 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Recipes : MonoBehaviour
 {
+    public TextMeshProUGUI hint; 
+
     private Inventory inventory;
 
-    void Start() {
+    void Start() 
+    {
         inventory = GameObject.Find("GameManager").GetComponent<Inventory>();
+
+
+        Item meat = (Item)ScriptableObject.CreateInstance("Item");
+        meat.name = "Meat";
+        inventory.Add(meat);
     }
 
-    public void Test() {
+    Item FindItem(string name) 
+    {
+        // Check if player has item of given name and amount
+        foreach (Item item in inventory.items) {
+            if (item.name == name) {
+                return item;
+            }
+        }   
+        return null;
+    }
+
+    void ShowHint() 
+    {
+        hint.text = "You don't have the required ingredients to cook that!";
+        hint.gameObject.SetActive(true); 
+        Invoke(nameof(HideHint), 1f); 
+    }
+
+    void HideHint()
+    {
+        hint.gameObject.SetActive(false); 
+    }
+
+    public void Test() 
+    {
         Debug.Log("Click caputed!");
+    }
+
+    public void SteakRecipe() 
+    {
+        Item meat = FindItem("Meat");
+        if (meat != null) {
+            inventory.Remove(meat);
+        }
+        else {
+            ShowHint();
+        }
     }
 }
